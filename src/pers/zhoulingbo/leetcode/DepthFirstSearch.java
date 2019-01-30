b2a943b4d5154c83de65d23d047fea562b99f446
zlb <253066156@qq.com>
2019-01-30 11:20:02 +0800

    增加108等

--- src/pers/zhoulingbo/leetcode/DepthFirstSearch.java
+++ src/pers/zhoulingbo/leetcode/DepthFirstSearch.java
@@ -1,5 +1,8 @@
 package pers.zhoulingbo.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 
  * 深度优先搜索
@@ -11,7 +14,87 @@ public class DepthFirstSearch
 
     public static void main(String[] args)
     {
+        TreeNode n = new TreeNode(1);
+        n.left = new TreeNode(2);
+        n.right = new TreeNode(3);
+        n.left.left = new TreeNode(4);
+        n.left.right = new TreeNode(5);
+        n.right.left = new TreeNode(6);
+        n.right.right = new TreeNode(7);
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue = inorderTraversal(n, queue);
+        while (!queue.isEmpty())
+        {
+            System.out.println(queue.poll());
+        }
+    }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树(convert-sorted-array-to-binary-search-tree)，
+     * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+     * @param nums
+     * @return
+     */
+    public static TreeNode sortedArrayToBST(int[] nums)
+    {
+        if (nums == null || nums.length == 0)
+            return null;
+        if (nums.length == 1)
+            return new TreeNode(nums[0]);
+
+        int index = nums.length / 2;
+        TreeNode node = new TreeNode(nums[index]);
+        node.left = sortedArrayToBST(nums, 0, index - 1);
+        node.right = sortedArrayToBST(nums, index + 1, nums.length - 1);
+        return node;
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums, int start, int end)
+    {
+        if (end < start)
+            return null;
+        if (end == start)
+            return new TreeNode(nums[start]);
+
+        int index = (end + start) / 2;
+        TreeNode node = new TreeNode(nums[index]);
+        node.left = sortedArrayToBST(nums, start, index - 1);
+        node.right = sortedArrayToBST(nums, index + 1, end);
+        return node;
+    }
+
+    /**
+     * 104. 二叉树的最大深度(maximum-depth-of-binary-tree)
+     * @param root
+     * @return
+     */
+    public static int maxDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        int depth = 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        return depth;
+    }
+
+    /**
+     * 559. N叉树的最大深度(maximum-depth-of-n-ary-tree)
+     * @param root
+     * @return
+     */
+    public int maxDepth(Node root)
+    {
+        if (root == null)
+            return 0;
+
+        int subDepth = 0;
+        for (Node node : root.children)
+        {
+            subDepth = Math.max(subDepth, maxDepth(node));
+        }
+        int depth = 1 + subDepth;
+        return depth;
     }
 
     /**
@@ -21,7 +104,8 @@ public class DepthFirstSearch
      */
     public static boolean judgePoint24(int[] nums)
     {
-
+        if (nums[0] + nums[1] + nums[2] + nums[3] == 24)
+            return true;
         return true;
     }
 
@@ -56,5 +140,42 @@ public class DepthFirstSearch
             image = floodFill(image, sr + 1, sc, newColor);
 
         return image;
+    }
+
+    /**
+     * 897. 递增顺序查找树(increasing-order-search-tree)
+     * 给定一个树，按中序遍历重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点.
+     * @param root
+     * @return
+     */
+    public static TreeNode increasingBST(TreeNode root)
+    {
+        if (root == null)
+            return null;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue = inorderTraversal(root, queue);
+
+        TreeNode tree = new TreeNode(queue.poll());
+        TreeNode temp = tree;
+        while (!queue.isEmpty())
+        {
+            TreeNode node = new TreeNode(queue.poll());
+            temp.right = node;
+            temp = node;
+        }
+        return tree;
+    }
+
+    public static Queue<Integer> inorderTraversal(TreeNode root, Queue<Integer> queue)
+    {
+        if (root == null)
+            return queue;
+
+        inorderTraversal(root.left, queue);
+        queue.add(root.val);
+        inorderTraversal(root.right, queue);
+
+        return queue;
     }
 }
---
+++ src/pers/zhoulingbo/leetcode/Node.java
@@ -0,0 +1,32 @@
+/*
+ * 版权所有 (C) 2015 知启蒙(ZHIQIM) 保留所有权利。
+ * 
+ * 指定登记&发行网站： https://www.zhiqim.com/ 欢迎加盟知启蒙，[编程有你，知启蒙一路随行]。
+ *
+ * 本文采用《知启蒙许可证》，除非符合许可证，否则不可使用该文件！
+ * 1、您可以免费使用、修改、合并、出版发行和分发，再授权软件、软件副本及衍生软件；
+ * 2、您用于商业用途时，必须在原作者指定的登记网站，按原作者要求进行登记；
+ * 3、您在使用、修改、合并、出版发行和分发时，必须包含版权声明、许可声明，及保留原作者的著作权、商标和专利等知识产权；
+ * 4、您在互联网、移动互联网等大众网络下发行和分发再授权软件、软件副本及衍生软件时，必须在原作者指定的发行网站进行发行和分发；
+ * 5、您可以在以下链接获取一个完整的许可证副本。
+ * 
+ * 许可证链接：http://zhiqim.org/licenses/zhiqim_register_publish_license.htm
+ * 
+ * 除非法律需要或书面同意，软件由原始码方式提供，无任何明示或暗示的保证和条件。详见完整许可证的权限和限制。
+ */
+package pers.zhoulingbo.leetcode;
+
+import java.util.List;
+
+public class Node
+{
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
