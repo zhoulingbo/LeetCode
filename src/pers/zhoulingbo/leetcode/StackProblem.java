e370218034b5ba8c8d8343ffec6280526137f9ec
zlb <253066156@qq.com>
2019-03-05 09:15:08 +0800

    856. 括号的分数

--- src/pers/zhoulingbo/leetcode/StackProblem.java
+++ src/pers/zhoulingbo/leetcode/StackProblem.java
@@ -13,10 +13,16 @@ import java.util.Stack;
 public class StackProblem
 {
 
+    public static void main(String[] args)
+    {
+        int v = scoreOfParentheses("(()((()((())))(())()(()()()())()()())((())))");
+        System.out.println(v);
+    }
+
     /**
      * 144. 二叉树的前序遍历(binary-tree-preorder-traversal)
      */
-    public List<Integer> preorderTraversal(TreeNode root)
+    public static List<Integer> preorderTraversal(TreeNode root)
     {
         List<Integer> list = new ArrayList<>();
         if (root == null)
@@ -36,5 +42,67 @@ public class StackProblem
         }
 
         return list;
+    }
+
+    /**
+     * 856. 括号的分数
+     * @param S
+     * @return
+     */
+    public static int scoreOfParentheses(String S)
+    {
+        char digit = 'd';
+        Stack<Character> stack = new Stack<>();
+        Stack<Integer> values = new Stack<>();
+        char[] chars = S.toCharArray();
+        for (char ch : chars)
+        {
+            if (stack.isEmpty() || '(' == ch)
+            {
+                stack.push(ch);
+                continue;
+            }
+
+            if (')' == ch)
+            {
+                char c = stack.peek();
+                if ('(' == c)
+                {
+                    stack.pop();
+                    if (!stack.isEmpty() && stack.peek() == digit)
+                    {// 如果上一个为数字，则+
+                        int a = values.pop();
+                        a += 1;
+                        values.push(a);
+                    }
+                    else
+                    {// 如果为非数字，则入栈
+                        stack.push(digit);
+                        values.push(1);
+                    }
+                }
+                else
+                {
+                    int a = values.pop();
+                    a *= 2;
+                    stack.pop();    // 弹出'd'
+                    stack.pop();    // 弹出'('
+                    if (!stack.isEmpty() && stack.peek() == digit)
+                    {// 如果上一个为数字，则+
+                        int b = values.pop();
+                        b += a;
+                        values.push(b);
+                        stack.pop();
+                        stack.push(digit);
+                    }
+                    else
+                    {
+                        values.push(a);
+                        stack.push(digit);
+                    }
+                }
+            }
+        }
+        return values.pop();
     }
 }
