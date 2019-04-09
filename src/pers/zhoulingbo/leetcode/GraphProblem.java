43f04a04e2d9063ec9a73c2d5e3dc7934ce5cf6d
zlb <253066156@qq.com>
2019-04-09 11:26:44 +0800

    增加除法求值

--- src/pers/zhoulingbo/leetcode/GraphProblem.java
+++ src/pers/zhoulingbo/leetcode/GraphProblem.java
@@ -1,17 +1,215 @@
 package pers.zhoulingbo.leetcode;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 
+/**
+ * 
+ * 图问题求解
+ * 
+ */
 public class GraphProblem
 {
 
     public static void main(String[] args)
     {
+//        int[][] prerequisites = new int[][] {{ },  { 3 }, {  }, { 1 }, { } };
+//        System.out.println(isBipartite(prerequisites));
 
+    }
+
+    /**
+     * 399. 除法求值
+     * @param equations
+     * @param values
+     * @param queries
+     * @return
+     */
+    public static double[] calcEquation(String[][] equations, double[] values, String[][] queries)
+    {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < equations.length; i++)
+        {
+            set.add(equations[i][0]);
+            set.add(equations[i][1]);
+        }
+        double[] a = new double[queries.length];
+        for (int i = 0; i < queries.length; i++)
+        {
+            String first = queries[i][0];
+            String second = queries[i][1];
+            if (first.equals(second))
+            {
+                if (set.contains(first))
+                    a[i] = 1.0;
+                else
+                    a[i] = -1.0;
+                continue;
+            }
+
+            Map<String, Double> map = new HashMap<>();
+            map.put(first, 1.0);
+            while (true)
+            {
+                boolean hasNew = false;
+                boolean found = false;
+                for (int j = 0; j < equations.length; j++)
+                {
+                    String f = equations[j][0];
+                    String s = equations[j][1];
+                    if (map.containsKey(f))
+                    {
+                        double val = map.get(f) * values[j];
+                        if (!map.containsKey(s))
+                        {
+                            map.put(s, val);
+                            hasNew = true;
+                        }
+                        if (s.equals(second))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                    else if (map.containsKey(s))
+                    {
+                        double val = map.get(s) / values[j];
+                        if (!map.containsKey(f))
+                        {
+                            map.put(f, val);
+                            hasNew = true;
+                        }
+                        if (f.equals(second))
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (found || !hasNew)
+                    break;
+            }
+            
+            if (map.containsKey(second))
+                a[i] = map.get(second);
+            else
+                a[i] = -1.0;
+        }
+
+        return a;
+    }
+
+    /**
+     * 207. 课程表
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public static boolean canFinish(int numCourses, int[][] prerequisites)
+    {
+        int[] a = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++)
+        {
+            int[] item = prerequisites[i]; // 需要先决条件
+            a[item[0]] = 1; // 1表示需先决条件,0表示不需要
+        }
+
+        for (int i = 0; i < a.length; i++)
+        {
+            if (a[i] == 0)
+                continue;
+
+            Queue<Integer> queue = new LinkedList<>();
+            for (int j = 0; j < prerequisites.length; j++)
+            {
+                if (prerequisites[j][0] == i)
+                {
+                    queue.add(prerequisites[j][1]);
+                }
+            }
+        }
+
+        for (int i = 0; i < a.length; i++)
+        {
+            if (a[i] == 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 785. 判断二分图
+     * @param graph
+     * @return
+     */
+    public static boolean isBipartite(int[][] graph)
+    {
+        Set<Integer> red = new HashSet<>();
+        Set<Integer> blue = new HashSet<>();
+        Set<Integer> disperse = new HashSet<>();
+
+        for (int i = 0; i < graph.length; i++)
+        {
+            if (graph[i].length == 0)
+            {
+                disperse.add(i);
+                continue;
+            }
+            
+            if (!red.contains(i))
+            {
+                red.add(i);
+                boolean b = color(graph, red, blue, i);
+                if (!b)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+    
+    private static boolean color(int[][] graph, Set<Integer> red, Set<Integer> blue, int r)
+    {
+        Queue<Integer> queueRed = new LinkedList<>();
+        Queue<Integer> queueBlue = new LinkedList<>();
+        queueRed.add(r);
+        while (!queueRed.isEmpty() && !queueBlue.isEmpty())
+        {
+            while (!queueRed.isEmpty())
+            {
+                int i = queueRed.poll();
+                for (int j = 0; j < graph[i].length; j++)
+                {
+                    if (red.contains(graph[i][j]))
+                        return false;
+                    blue.add(graph[i][j]);
+                    queueBlue.add(graph[i][j]);
+                }
+            }
+            
+            while (!queueBlue.isEmpty())
+            {
+                int i = queueBlue.poll();
+                for (int j = 0; j < graph[i].length; j++)
+                {
+                    if (blue.contains(graph[i][j]))
+                        return false;
+                    red.add(graph[i][j]);
+                    queueRed.add(graph[i][j]);
+                }
+            }
+        }
+        
+        return true;
     }
 
     /**
@@ -116,7 +314,7 @@ public class GraphProblem
             }
         }
 
-        for (int i=0 ;i<b.length; i++)
+        for (int i = 0; i < b.length; i++)
             if (!b[i])
                 return false;
 
